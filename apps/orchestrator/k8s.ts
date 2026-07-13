401c4860e8b615edbb94b107559fbc24b6a448b2
jitendarmahara <jitendarmahara2002@gmail.com>
2026-07-13 11:26:02 +0530

    remove agents prototype loop and annotate k8s client

--- apps/orchestrator/k8s.ts
+++ apps/orchestrator/k8s.ts
@@ -2,3 +2,5 @@ import * as k8s from "@kubernetes/client-node";
 const kc  = new k8s.KubeConfig();
 kc.loadFromCluster(); // this is when then orchestrator is deployed
 export const objectApi = k8s.KubernetesObjectApi.makeApiClient(kc);
+
+// what i understand this whole file tells the kubertanc authentaic me once and then after  i will tell u what u have to do for me ;
